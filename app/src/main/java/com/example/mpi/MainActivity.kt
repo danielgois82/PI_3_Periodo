f3a82d626109422b86b53e613b335c8d923c4e6f
Vinícius <vinibsilva32@gmail.com>
2025-05-03 21:40:22 -0300

    commit inicial na branch vinicius-barbosa

--- app/src/main/java/com/example/mpi/MainActivity.kt
+++ app/src/main/java/com/example/mpi/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.example.mpi
 
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mpi.ui.cadastroAcao
+import com.example.mpi.ui.cadastroAtividade
+import com.example.mpi.ui.cadastroPilar
+import com.example.mpi.ui.cadastroSubPilar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+        val cadPilar: Button = findViewById(R.id.cadastroPilar)
+        cadPilar.setOnClickListener {
+            val intent = Intent(this, cadastroPilar::class.java)
+            startActivity(intent)
+        }
+        val cadSubPilar: Button = findViewById(R.id.cadastroSubPilar)
+        cadSubPilar.setOnClickListener {
+            val intent = Intent(this, cadastroSubPilar::class.java)
+            startActivity(intent)
+        }
+
+        val cadAcao: Button = findViewById(R.id.cadastroAcao)
+        cadAcao.setOnClickListener {
+            val intent = Intent(this, cadastroAcao::class.java)
+            startActivity(intent)
+        }
+
+        val cadAtividade: Button = findViewById(R.id.cadastroAtividade)
+        cadAtividade.setOnClickListener {
+            val intent = Intent(this, cadastroAtividade::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/mpi/data/PilarContract.kt
@@ -0,0 +1,17 @@
+package com.example.mpi.data
+import android.provider.BaseColumns
+
+object PilarContract {
+    object UserEntry : BaseColumns {
+        const val TABLE_NAME = "pilar"
+        const val COL_NOME   = "nome"
+        const val COL_DESCRICAO  = "descricao"
+        const val COL_RESPONSAVEL  = "codResponsavel"
+        const val COL_DATAI   = "dataInicio"
+        const val COL_DATAT   = "dataTermino"
+        const val COL_APROVADO   = "isAprovado"
+        const val COL_FINALIZADO   = "isFinalizado"
+        const val COL_PERCENTUAL = "percentual"
+    }
+}
+
