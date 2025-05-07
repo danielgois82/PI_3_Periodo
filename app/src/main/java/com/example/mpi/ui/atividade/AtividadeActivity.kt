6b767793b06ef1040c867e39c9373a0822c4d211
Vinícius <vinibsilva32@gmail.com>
2025-05-07 10:20:06 -0300

    Adicionando o CRUD da tabela pilares + visualização das telas de cadastro para pilares, subpilares, ações e atividades.

---
+++ app/src/main/java/com/example/mpi/ui/atividade/AtividadeActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mpi.ui.atividade
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mpi.R
+import com.example.mpi.ui.acao.cadastroAcao
+
+class AtividadeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_atividade)
+
+        val cadAtividade: Button = findViewById(R.id.btnCadastrarAtividade)
+        cadAtividade.setOnClickListener {
+            val intent = Intent(this, cadastroAtividade::class.java)
+            startActivity(intent)
+        }
+        val voltar: ImageView = findViewById(R.id.btnVoltar)
+        voltar.setOnClickListener {
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
