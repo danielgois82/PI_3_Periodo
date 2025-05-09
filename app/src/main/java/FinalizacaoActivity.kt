c8c692aa8472416079c5f5d8837d7b670d2a7005
Pedro Asafe <asafepedro665@gmail.com>
2025-05-09 01:06:09 -0300

    telas de aprovação e finalização

---
+++ app/src/main/java/FinalizacaoActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mpi
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mpi.databinding.ActivityFinalizacaoBinding
+
+class FinalizacaoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFinalizacaoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFinalizacaoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnVoltarInicio.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+    }
+}
